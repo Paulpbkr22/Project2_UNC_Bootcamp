b67251b5fed449eb29da9ff8282e6aaf9912bc86
Jerrica VanAlstyne <jsnow07@yahoo.com>
2020-01-04 14:11:59 -0500

    added user creation

--- routes/createseeds.js
+++ routes/createseeds.js
@@ -2,7 +2,11 @@ var create = require("../seeders/20191223212251-demo--user");
 module.exports = function(app){
 app.post("/createseeds", function(req, res){
     create.createSeeds(req, function(dbInvite){
+
+        
         res.send(dbInvite);
+
+
     });
 });
 }
