199667cb83c950a70d54aadf3c743a4b26c258bf
Jerrica VanAlstyne <jsnow07@yahoo.com>
2020-01-06 17:18:11 -0500

    updated route that the invite checks when creating a user

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,27 +17,27 @@ module.exports = function(app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
-    console.log(req.body);
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password,
-      name: req.body.name,
-      jobTitle: req.body.jobTitle
-    }).then(function() {
-      res.redirect(307, "/api/login");
-    }).catch(function(err) {
-      console.log(err);
-      // res.json(err);
-      console.log("Failed");
-      // $("#registrationModal").modal("toggle");
-      // res.send(err);
-      // res.sendFile(path.join(__dirname, "../public/HTML/registration.html"));
+  // app.post("/api/signup", function(req, res) {
+  //   console.log(req.body);
+  //   db.User.create({
+  //     email: req.body.email,
+  //     password: req.body.password,
+  //     name: req.body.name,
+  //     jobTitle: req.body.jobTitle
+  //   }).then(function() {
+  //     res.redirect(307, "/api/login");
+  //   }).catch(function(err) {
+  //     console.log(err);
+  //     // res.json(err);
+  //     console.log("Failed");
+  //     // $("#registrationModal").modal("toggle");
+  //     // res.send(err);
+  //     // res.sendFile(path.join(__dirname, "../public/HTML/registration.html"));
       
-      // res.redirect("/");
-      res.status(422).json(err.errors[0].message);
-    });
-  });
+  //     // res.redirect("/");
+  //     res.status(422).json(err.errors[0].message);
+  //   });
+  // });
 
   // Route for logging user out
   app.get("/logout", function(req, res) {
